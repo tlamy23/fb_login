0c16099c355db9176ac349e0c24816ef5771f75a
mitzy <tlamy23@hotmail.com>
2014-02-18 17:09:31 -0800

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FbLogin::Application.config.secret_key_base = '33db0966d03a6f141ad5d4b158d4a5292a27364f1c2c1c75c491d4bd67b9061e02f4c396acdd54c90a589bcd0dc0a03252b2ddaf61b0001aa1fd7a7cb582d358'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FbLogin::Application.config.session_store :cookie_store, key: '_fb_login_session'
